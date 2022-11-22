0a0992f03cff211c1224313581fb45c616475bd6
Swanand Mathekar <swanandmathekar@gmail.com>
2022-11-23 00:27:31 +0530

    DB settings fix

--- psocial_api/settings.py
+++ psocial_api/settings.py
@@ -83,19 +83,19 @@ WSGI_APPLICATION = 'psocial_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         default='postgresql://postgres:postgres@localhost:5432/psocial_api',
-#         conn_max_age=600
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:5432/psocial_api',
+        conn_max_age=600
+    )
+}
 
 
 # Password validation
